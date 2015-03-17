348e5935dff2a8d7e8f23fa56f7697c5c5cf23bd
memoi2001 Fooker <memoi2001@fooker.fr>
2015-03-17 10:52:50 +0100

    IP finished, weekends in place, starting on national holidays

--- scripts/business.py
+++ scripts/business.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python
+
+import datetime
+
+def isBusinessDay():
+	today = datetime.date.today()
+	if (today.weekday() >= 5 ):
+		return False
+	else:
+		print "weekday"
+		if ( isholiday(today) ):
+			return False
+		else:
+			return True

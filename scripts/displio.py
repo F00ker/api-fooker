9d83d47737317a419feb67eef41f353bf30769c0
f00ker <memoi2001@fooker.fr>
2016-06-25 15:27:39 +0200

    Added free space on /home

--- scripts/displio.py
+++ scripts/displio.py
@@ -1,12 +1,15 @@
 import os, sys
 from datetime import datetime
 from datetime import timedelta
+from collections import namedtuple
 
 #######################################
 #
 # Content for my displio
 #
 #######################################
+
+_ntuple_diskusage = namedtuple('usage', 'total used free')
 
 def displiocontent():
     time = datetime.now().strftime("%a %d/%m/%y - %H:%M")
@@ -19,4 +22,15 @@ def displiocontent():
     raw_average = os.getloadavg()
     load_average = raw_average[0]
 
-    return "--- FOOKER ---\nTime:\n{}\n\nUptime:\n{}\n\nLoad:{}".format(time,formated_uptime,load_average)
+    return "--- FOOKER ---\nTime:\n{}\n\nUptime:\n{}\n\nLoad:{}\n\nFree Space: {}GB".format(time,formated_uptime,load_average,disk_usage())
+
+
+def disk_usage():
+    """Return disk usage statistics about the given path.
+
+    Returned valus is a named tuple with attributes 'total', 'used' and
+    'free', which are the amount of total, used and free space, in bytes.
+    """
+    st = os.statvfs("/home")
+    free = st.f_bavail * st.f_frsize
+    return free / 1024 / 1024 / 1024

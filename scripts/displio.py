ca10bf4d93787a3637c32361c6b73631f28b8165
f00ker <memoi2001@fooker.fr>
2016-06-25 14:59:05 +0200

    Displio monitoring setup

--- scripts/displio.py
+++ scripts/displio.py
@@ -1,4 +1,6 @@
-#!/bin/bash
+import os, sys
+from datetime import datetime
+from datetime import timedelta
 
 #######################################
 #
@@ -6,6 +8,15 @@
 #
 #######################################
 
-
 def displiocontent():
-    return "Probably should but some code here...."
+    time = datetime.now().strftime("%a %d/%m/%y - %H:%M")
+
+    with open('/proc/uptime', 'r') as f:
+        uptime_seconds = float(f.readline().split()[0])
+        uptime_string = str(timedelta(seconds = uptime_seconds))
+        formated_uptime = uptime_string.split('.')[0]
+
+    raw_average = os.getloadavg()
+    load_average = raw_average[0]
+
+    return "--- FOOKER ---\nTime:\n{}\n\nUptime:\n{}\n\nLoad:{}".format(time,formated_uptime,load_average)
